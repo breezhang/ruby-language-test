4166103ef342272ab76501c530d7828d232ab05c
breezang1877@hotmail.com <breezang1877@hotmail.com>
2014-09-03 21:46:14 +0800

    ruby array own pack toolkit

--- Array/Set_API_A.rb
+++ Array/Set_API_A.rb
@@ -8,7 +8,7 @@ class Program_Run_Set
 
   mem1 = 0..127
 
-  mem2 = %w{ cat sheep bear} #string set
+  mem2 = %w{ lazy dog jumped over quick brown fox} #string set
 
 
   mem3 = mem1.map { |x| x.chr } # map
@@ -19,7 +19,44 @@ class Program_Run_Set
 
   ap mem1.select { |x| x %2==0 } #select
 
-  ap (mem3.grep(/[\x00-\x7F]/)).select { |x| x =~/[\000-~]/ } #perl [ -~] work but ruby not \x \000
+  ap (mem3.grep(/[\x00-\x7F]/)).select { |x| x =~/[\000-~]/ } #perl [ -~] work but ruby not  fix through \x \000
+
+  ap mem3.map { |x| x.gsub(/[\x00-\x7F]/) { |y| y.ord } }
+
+  ap %x(ls)
+
+  ap "lazy dog jumped over quick brown fox".split(' ') #use split easy get array object
+
+  p mem2.size #??
+
+
+  p 'ruby has many verb operator set eg: select add select uniq first find'
+
+  ap mem3.collect { |x| x.ord }
+
+  p "perl own very useful toolkit 'pack' 'unpack' ruby also own same "
+
+
+  ap mem3.collect { |x| x.unpack('C')[0] }
+
+  p "see some thing"
+
+  mem2.each_index { |x| p mem2[x] if x %2 ==0 }
+
+
+  p "stack operator"
+
+  # ap  mem2.pop
+
+  p "query operator"
+
+  ap mem2.rotate
+
+  ap mem2.unshift("dog")
+
+  p "has some stuff"
+
+  ap [1, 2, 3].zip([4, 5, 6]).flatten
 
 
 end
